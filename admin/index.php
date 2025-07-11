0ee281b8fbb76d710bf18eac51cf81680c1770c8
nabin1155 <nabinsingh300@gmail.com>
2025-07-11 17:20:43 +0545

    fix the design

---
+++ admin/index.php
@@ -0,0 +1,44 @@
+<?php include('common/header.php') ?>
+
+     <!-- Body Section Starts -->
+     <section class="content">
+        <div class="wrapper">
+            <h1 class="heading">DASHBOARD</h1>
+            <br>
+            <?php 
+                include('config/session.php');
+            ?>
+            <div class="clearfix"></div>
+           
+            <div class="col-4 text-center">
+                <h2>4</h2>
+                <br>
+                <h2>Users</h2>
+            </div>
+
+            <div class="col-4 text-center">
+                <h2>4</h2>
+                <br>
+                <h2>Categories</h2>
+            </div>
+
+            <div class="col-4 text-center">
+                <h2>5</h2>
+                <br>
+                <h2>Products</h2>
+            </div>
+
+            <div class="col-4 text-center">
+                <h2>4</h2>
+                <br>
+                <h2>Orders</h2>
+            </div>
+
+            <div class="clearfix"></div>
+
+           </div>
+        </div>
+    </section>
+    <!-- Body Section Ends -->
+
+<?php include('common/footer.php') ?>
